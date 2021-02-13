2df2855602729600d6a60933015400e758f69a34
seojung94 <73627443+seojung94@users.noreply.github.com>
2021-02-13 22:36:08 +0900

    response ->JSON

--- vanilla/weather.js
+++ vanilla/weather.js
@@ -19,7 +19,7 @@ function getWeather(lat, lng){
 }
 
 function saveCoords(coordsObj){
-    localStorage.setItem(COORDS, response.stringify(coordsObj));
+    localStorage.setItem(COORDS, JSON.stringify(coordsObj));
 }
 
 function handleGeoSuccess(position){
